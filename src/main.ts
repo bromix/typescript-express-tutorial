78eb2ad368e1deeb4cbfdb7881f6c1bdb319014b
Matthias Bromisch <mbr@ashampoo.com>
2021-04-17 16:16:26 +0200

    [add] initial

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+/*
+PORT could be set via environment variable (e.g. by Google Cloud
+or some other could service). Fallback to your desired PORT.
+*/
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  // only log this information in development.
+  if (process.env?.NODE_ENV !== "production")
+    console.log(`server listening at http://localhost:${PORT}`);
+});
